f45070913d25e11b9678f8fe71f22d2a60c57d6e
Volodymyr L <northwoodzp@gmail.com>
2024-07-27 22:01:47 +0300

    L7 4:00

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace project
 {
     class Program
     {
         public static void Main()
+        {
+            // DataTypes();
+            // MathOperations();
+            // ConditionalConstructions();
+            // SwitchCaseOperator();
+            Loops();
+        }
+
+        public static void DataTypes()
         {
             #region WholeNumbers
 
@@ -52,6 +62,99 @@ namespace project
             user2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("User1 res: " + user1 + " <-> User2 res: " + user2);
+        }
+
+        public static void MathOperations()
+        {
+            Console.Write("Enter number: ");
+            float userInput = float.Parse(Console.ReadLine());
+
+            float result = userInput + 10f;
+            result = userInput - 10f;
+            result = userInput * 10f;
+            result = userInput / 10f;
+            result = userInput % 10f;
+
+            result += 5f;
+            result -= 6;
+            result++;
+            result--;
+
+
+            Console.WriteLine("Result: " + result);
+
+            Console.WriteLine("Enter radius: ");
+
+            float radius = float.Parse(Console.ReadLine());
+
+            double area = Math.PI * Math.Pow(radius, 2);
+
+            Console.WriteLine("Round area: " + area);
+        }
+
+        public static void ConditionalConstructions()
+        {
+            Console.WriteLine("Enter number: ");
+            short input = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter randomly 0 or 1");
+            bool boolInput = Convert.ToBoolean(Console.ReadLine());
+
+            if (input == 12)
+                Console.WriteLine("yes, correct");
+            else if ((input > 5 && boolInput) || (input < 5 && !boolInput))
+                Console.WriteLine("Complex condition run");
+            else if (input < 16)
+                Console.WriteLine("Close to correct");
+            else
+                Console.WriteLine("Completely wrong");
+        }
+
+        public static void SwitchCaseOperator()
+        {
+            Console.WriteLine("Enter name:");
+
+            string name = Console.ReadLine();
+
+            switch (name)
+            {
+                case "test":
+                    Console.WriteLine("Name is test");
+                    break;
+                case "qwerty":
+                    Console.WriteLine("Qwerty is set");
+                    break;
+                default:
+                    Console.WriteLine("default handler");
+                    break;
+            }
+        }
+
+        public static void Loops()
+        {
+            for (byte i = 0; i < 19; i++)
+            {
+                Console.WriteLine("For iteration # {0}", i + 1);
+            }
+
+            byte whileCount = 10;
+            byte whileIterator = 0;
+
+            while (whileIterator < whileCount)
+            {
+                if (whileIterator == 5) continue;
+                Console.WriteLine("While iteration # {0}", whileIterator + 1);
+                whileIterator++;
+            }
+
+            byte doCount = 12;
+            byte doIterator = 0;
+
+            do
+            {
+                Console.WriteLine("Do iterator # {0}", doIterator + 1);
+                doIterator++;
+            } while (doIterator < doCount);
         }
     }
 }
